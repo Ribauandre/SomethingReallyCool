3faec0bf03caa7a41fa5ac6ce20e0273543764b8
Andre Ribau <Ribauandre@yahoo.com>
2024-02-02 13:31:23 -0500

    messing around

--- smc/src/App.js
+++ smc/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Currently building something cool</h1>
+          <h1>Currently showing jorge something cool?</h1>
         </header>
       </div>
     );
